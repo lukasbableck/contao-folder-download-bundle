f9c5f96f28afc8aee858ac50739c223ef9e94f9c
Lukas Bableck <lukas@bableck.dev>
2024-06-24 09:08:06 +0200

    implement zip download

--- src/EventListener/DataContainer/AddOperationsListener.php
+++ src/EventListener/DataContainer/AddOperationsListener.php
@@ -4,9 +4,12 @@ namespace Lukasbableck\ContaoFolderDownloadBundle\EventListener\DataContainer;
 
 use Contao\Backend;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\CoreBundle\Exception\ResponseException;
 use Contao\FilesModel;
 use Contao\Input;
-use Contao\System;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\String\UnicodeString;
 
 #[AsHook('loadDataContainer')]
 class AddOperationsListener extends Backend {
@@ -22,26 +25,43 @@ class AddOperationsListener extends Backend {
 		];
 
 		if (Input::get('key') === 'download' && Input::get('id')) {
-			$file = FilesModel::findByPk(Input::get('id'));
-			if ($file !== null) {
-				if ($file->type == 'folder') {
-					$projectDir = System::getContainer()->getParameter('kernel.project_dir');
+			$objFile = FilesModel::findByPath(Input::get('id'));
+			if ($objFile !== null) {
+				if ($objFile->type == 'folder') {
+					$temp = tempnam(sys_get_temp_dir(), 'CFD');
 					$zip = new \ZipArchive();
-					$zip->open($projectDir.'/'.$file->path.'.zip', \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+					$zip->open($temp, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
 					$files = new \RecursiveIteratorIterator(
-						new \RecursiveDirectoryIterator($projectDir.'/'.$file->path),
+						new \RecursiveDirectoryIterator($objFile->getAbsolutePath()),
 						\RecursiveIteratorIterator::LEAVES_ONLY
 					);
 					foreach ($files as $name => $file) {
 						if (!$file->isDir()) {
 							$filePath = $file->getRealPath();
-							$relativePath = substr($filePath, \strlen($projectDir.'/'.$file->path) + 1);
+							$relativePath = substr($filePath, \strlen($file->getPath()) + 1);
 							$zip->addFile($filePath, $relativePath);
-						}else{
+						} else {
 							$zip->addEmptyDir($file->getRealPath());
 						}
 					}
-					$this->sendFileToBrowser($file->path.'.zip');
+					$zip->close();
+
+					$response = new BinaryFileResponse($temp);
+					$response->setPrivate();
+					$response->setAutoEtag();
+
+					$response->setContentDisposition(
+						ResponseHeaderBag::DISPOSITION_INLINE,
+						$objFile->name.'.zip',
+						(new UnicodeString($objFile->name.'.zip'))->ascii()->toString()
+					);
+
+					$response->headers->addCacheControlDirective('must-revalidate');
+					$response->headers->set('Connection', 'close');
+					$response->headers->set('Content-Type', 'application/zip');
+
+					throw new ResponseException($response);
+					unlink($temp);
 				}
 			}
 		}
